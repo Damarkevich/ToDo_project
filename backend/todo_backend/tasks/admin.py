5df6533c080ab79e5f6db47b1541039c3adde492
Dmitrii Markevich <markevich.dmitry@gmail.com>
2023-03-23 16:24:44 +0100

    Delete backend directory

--- backend/todo_backend/tasks/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Task
-
-
-class TaskAdmin(admin.ModelAdmin):
-    list = ('title', 'description', 'completed')
-
-
-admin.site.register(Task, TaskAdmin)
